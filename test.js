89c2113086f2eda464ef6119140b80ca973dded9
coloradokim <schlesinger.kim@gmail.com>
2016-07-29 21:55:04 -0600

    experimenting with Date getters

--- test.js
+++ test.js
@@ -2,13 +2,18 @@ var DateGenerator = function (date) {
   this.date = date
 }
 
-DateGenerator.prototype.getMonday= function(){
+DateGenerator.prototype.getMonday = function (){
   var startDate = new Date(this.date);
   var day = startDate.getDay();
+  console.log(startDate);
   var diffToMonday = startDate.getDate() - day + (day === 0 ? -6 : 1);
     return new Date(startDate.setDate(diffToMonday));
 }
 
-var test = new DateGenerator(new Date()).getMonday();
+DateGenerator.prototype.createWeeks = function () {
+
+}
+
+var test = new DateGenerator(new Date()).getMonday().getYear();
 
 console.log(test);
