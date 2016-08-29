7553acc20777953078eff6654bc62bcd5a0918ef
coloradokim <schlesinger.kim@gmail.com>
2016-08-29 16:46:07 -0600

    altered the format of the items in the return array

--- src/date.js
+++ src/date.js
@@ -1,5 +1,3 @@
-var moment = require('moment');
-
 function getWeeks(date) {
   var startDate = new Date(date);
   var day = date.getDay();
@@ -7,10 +5,11 @@ function getWeeks(date) {
   var firstMonday =  new Date(startDate.setDate(diffToMonday));
   var result = [];
   do {
-    result.push(firstMonday.setDate(firstMonday.getDate() + 7));
+    result.push(new Date(firstMonday.setDate(firstMonday.getDate() + 7)));
   } while (result.length < 52)
   return result;
 }
 console.log(getWeeks(new Date(2016, 06, 26)));
+
 
 module.exports = getWeeks
